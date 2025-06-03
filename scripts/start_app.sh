5d3a1c5c8a46f4984f5eeab4422373cc886aa02f
zzw890827 <zzw890827@gmail.com>
2025-06-03 12:16:01 +0900

    [Update] correct npm path.

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -3,11 +3,13 @@ set -e
 
 APP_DIR="/home/ubuntu/app"
 APP_NAME="focus-up-app"
+NVM_DIR="/home/ubuntu/.nvm"
+NODE_VERSION="v22.16.0"   # 与你实际安装的版本保持一致
 
 cd "$APP_DIR" || return
 npm install
 echo "使用 PM2 启动 NestJS 应用：npm run start:prod"
-pm2 start npm --name "$APP_NAME" -- run start:prod
+pm2 start "$NVM_DIR"/versions/node/"$NODE_VERSION"/bin/npm --name "$APP_NAME" -- run start:prod
 pm2 save
 
 echo "App 启动完成，进程名称：$APP_NAME"
